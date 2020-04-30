2bb78010e7c829c5ec155010a9df9562e55367a2
Ushahemba Shir <laleoict@gmail.com>
2020-04-30 22:26:44 +0100

    app working with ES5

--- app.js
+++ app.js
@@ -19,28 +19,35 @@ const row = document.createElement('tr'); // Create tr element
 list.appendChild(row);
 }
 
-// // Show Alert
-// UI.prototype.showAlert = function (message, className) {
-//     const div = document.createElement('div'); // Create div
-//     div.className = `alert ${className}`; // Add Classes
-//     div.appendChild(document.createTextNode(message)) //Add Text
-//     const container = document.querySelector('.container'); // Get Parent
-//     const form = document.querySelector('#sales-item');
-//     container.insertBefore(div, form); // Insert Alert
+// Show Alert
+UI.prototype.showAlert = function (message, className) {
+    const div = document.createElement('div'); // Create div
+    div.className = `alert ${className}`; // Add Classes
+    div.appendChild(document.createTextNode(message)) //Add Text
+    const container = document.querySelector('.container'); // Get Parent
+    const form = document.querySelector('#sales-entry'); // Get child
+    container.insertBefore(div, form); // Insert Alert
 
-//     setTimeout(function(){
-//         document.querySelector('.alert').remove();
-//     }, 3000);
-// }
+    setTimeout(function(){
+        document.querySelector('.alert').remove();
+    }, 2000);
+}
 
-// // Clear fields
-// UI.prototype.clearField = function(){
-//     document.getElementById('item').value = '';
-//     document.getElementById('qty').value = '';
-//     document.getElementById('amount').value = '';
-// }
+// Delete Sale prototype
+UI.prototype.deleteSale = function(target){
+    if (target.className === 'delete'){
+        target.parentElement.parentElement.remove();
+    }
+}
 
-// Event Listeners
+// Clear fields
+UI.prototype.clearFields = function(){
+    document.getElementById('item').value = '';
+    document.getElementById('qty').value = '';
+    document.getElementById('amount').value = '';
+}
+
+// Event Listeners for book entry
 document.getElementById('sales-entry').addEventListener('submit', 
 function (e) {
     // Get form values
@@ -51,13 +58,21 @@ function (e) {
 const sale = new Sale (item, qty, amount);  // Instanciate Book 
 
 const ui = new UI();  // Instanciate UI
-// if (item === '' || qty === '' || amount === '') { 
-//    ui.showAlert('Please fill in all fields');
-// } else {
+if (item === '' || qty === '' || amount === '') { 
+   ui.showAlert('Please enter all fields', 'error');
+} else {
     ui.addSaleToList(sale);  // Add book to list
-//     ui.showAlert('Bood Added!', 'success'); // Show success
-//     ui.clearField(); // Clear fields
-// }
+    ui.showAlert('Sale succesfully entered!', 'success'); // Show success
+    ui.clearFields(); // Clear fields after submit
+}
 
     e.preventDefault();
-});       
+});    
+
+// Event listener for delete
+document.getElementById('sale-list').addEventListener('click', function(e){
+    const ui = new UI() //Instantiate UI
+    ui.deleteSale(e.target); //Detete target
+    ui.showAlert('Sale removed', 'success') // Show delete alert
+    e.preventDefault();
+});
